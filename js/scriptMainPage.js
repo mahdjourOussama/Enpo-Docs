5c4ba1d18a50b864c06677702284ca1f0ae9372f
= <necibhamza2013@gmail.com>
2021-06-15 15:01:05 +0100

    fixing hamburger navbar

--- js/scriptMainPage.js
+++ js/scriptMainPage.js
@@ -2,13 +2,18 @@ const hamburger = document.getElementById('hamburger')
 hamburger.addEventListener('click', (e) => {
     let btn = e.target;
     let nav = document.querySelector(".navigation-bar")
+    let bigNavContainer = document.querySelector(".big-nav-container")
     let state = btn.getAttribute("state")
     nav.classList.toggle("open")
     if (state === "closed") {
         e.target.src = "./pics/close.png"
         btn.setAttribute("state", "open");
+        bigNavContainer.classList.add("header-hamburger-active")
+        nav.classList.add("hamburger-nav-active")
     } else {
         e.target.src = "./pics/hamburger-menu.png"
         btn.setAttribute("state", "closed");
+        bigNavContainer.classList.remove("header-hamburger-active")
+        nav.classList.remove("hamburger-nav-active")
     }
 })
